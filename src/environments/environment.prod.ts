78bf7b34e28e5edd5e0b3b8c5fc2c4bd6832e83a
shamsulazhar74 <shamsul.zhr@gmail.com>
2021-12-10 15:34:35 +0800

    initial update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCHMA6bF49YgruJle_Chnw4Pvz0bm2CNEw",
+    authDomain: "fbchat-1d2d5.firebaseapp.com",
+    projectId: "fbchat-1d2d5",
+    storageBucket: "fbchat-1d2d5.appspot.com",
+    messagingSenderId: "201491053916",
+    appId: "1:201491053916:web:66e6bb946a86bfb1a69599"
+    }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyCHMA6bF49YgruJle_Chnw4Pvz0bm2CNEw",
+    authDomain: "fbchat-1d2d5.firebaseapp.com",
+    projectId: "fbchat-1d2d5",
+    storageBucket: "fbchat-1d2d5.appspot.com",
+    messagingSenderId: "201491053916",
+    appId: "1:201491053916:web:66e6bb946a86bfb1a69599"
+    }
 };
 
 /*
